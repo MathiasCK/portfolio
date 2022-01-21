7b1f8828130a66da2133254b125723f727556a23
Mathias Kolberg <mathias.ck2000@gmail.com>
2022-01-21 16:28:01 +0100

    cryptoverse

---
+++ js/app.js
@@ -0,0 +1,171 @@
+const websites = [
+  {
+    title: 'Cryptoverse',
+    website: 'https://cryptoverse-frontend.vercel.app',
+    repo_name: 'cryptoverse-frontend',
+    mainImg: '../images/crypto-1.png',
+    url: 'project',
+    images: [
+      {
+        id: 2,
+        src: '../images/crypto-2.png',
+      },
+      {
+        id: 3,
+        src: '../images/crypto-3.png',
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+  },
+  {
+    title: 'Next.js eccomerce store',
+    repo_name: 'next-shop',
+    website: 'https://next-shop-iota.vercel.app',
+    mainImg: '../images/next-1.png',
+    url: '/project',
+    images: [
+      {
+        id: 2,
+        src: '../images/next-2.png',
+      },
+      {
+        id: 3,
+        src: '../images/next-3.png',
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+  },
+  {
+    title: 'Unsplash clone',
+    website: 'https://unsplashclone-one.vercel.app',
+    repo_name: 'unsplashclone',
+    mainImg: '../images/unsplash-1.png',
+    url: '/project',
+    images: [
+      {
+        id: 2,
+        src: '../images/unsplash-2.png',
+      },
+      {
+        id: 3,
+        src: '../images/unsplash-3.png',
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+  },
+  {
+    title: 'North media landing page',
+    website: 'https://media-phi.vercel.app/',
+    mainImg: '../images/north-1.png',
+    url: '/project',
+    images: [
+      {
+        id: 2,
+        src: '../images/north-2.png',
+      },
+      {
+        id: 3,
+        src: '../images/north-3.png',
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+  },
+  {
+    title: 'Crwn apperal',
+    repo_name: 'crwn-apperal',
+    website: 'https://crwn-apperal.herokuapp.com/',
+    mainImg: '../images/crwn-1.png',
+    url: '/project',
+    images: [
+      {
+        id: 2,
+        src: '../images/crwn-2.png',
+      },
+      {
+        id: 3,
+        src: '../images/crwn-3.png',
+      },
+    ],
+    description: [
+      {
+        title: 'Truly A masterpiece',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'Fresh look on a brutal sport.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+      {
+        title: 'It’s okay lmao.',
+        description:
+          '“Lorem Ipsum is simply dummy text of the printing and typesetting industry.”',
+      },
+    ],
+  },
+];
