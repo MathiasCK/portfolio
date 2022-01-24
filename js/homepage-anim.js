b809f383dfae42ebd5c06c01a587392d6c0f239b
Mathias Kolberg <mathias.ck2000@gmail.com>
2022-01-24 13:38:38 +0100

    Homepage animation

--- js/homepage-anim.js
+++ js/homepage-anim.js
@@ -6,12 +6,12 @@ textWrapper.innerHTML = textWrapper.textContent.replace(
 
 anime.timeline().add({
   targets: '.header-1 .letter',
-  translateY: [200, 0],
+  translateY: [50, 0],
   translateZ: 0,
   opacity: [0, 1],
   easing: 'easeOutExpo',
   duration: 2000,
-  delay: (el, i) => 4800 + 50 * i,
+  //delay: (el, i) => 4800 + 50 * i,
 });
 
 var textWrapper = document.querySelector('.header-2');
@@ -22,63 +22,50 @@ textWrapper.innerHTML = textWrapper.textContent.replace(
 
 anime.timeline().add({
   targets: '.header-2 .letter',
-  translateY: [200, 0],
+  translateY: [50, 0],
   translateZ: 0,
   opacity: [0, 1],
   easing: 'easeOutExpo',
   duration: 2000,
-  delay: (el, i) => 4800 + 50 * i,
+  delay: 1500,
 });
 
-TweenMax.to('.wrapper', 2, {
-  top: '-100%',
-  ease: Expo.easeInOut,
-  delay: 3.6,
+anime.timeline().add({
+  targets: '.text-1',
+  translateX: [200, 0],
+  translateZ: 0,
+  opacity: [0, 1],
+  easing: 'easeOutExpo',
+  duration: 2000,
+  delay: 2000,
 });
 
-var tl = new TimelineMax();
-
-tl.from('.loader', 1.6, {
-  scaleY: '0%',
-  y: 80,
-  ease: Expo.easeInOut,
-  delay: 1,
-  transformOrigin: '50% 100%',
+anime.timeline().add({
+  targets: '.text-2',
+  translateX: [200, 0],
+  translateZ: 0,
+  opacity: [0, 1],
+  easing: 'easeOutExpo',
+  duration: 2000,
+  delay: 2300,
 });
 
-tl.to('.loader', 1.6, {
-  height: '20vh',
-  scaleY: '0%',
-  ease: Expo.easeInOut,
-  transformOrigin: '0% -100%',
+anime.timeline().add({
+  targets: '.text-3',
+  translateX: [200, 0],
+  translateZ: 0,
+  opacity: [0, 1],
+  easing: 'easeOutExpo',
+  duration: 2000,
+  delay: 2400,
 });
 
-TweenMax.to('.box', 2.4, {
-  y: '-100%',
-  ease: Expo.easeInOut,
-  delay: 3.8,
+anime.timeline().add({
+  targets: '.text-4',
+  translateX: [200, 0],
+  translateZ: 0,
+  opacity: [0, 1],
+  easing: 'easeOutExpo',
+  duration: 2000,
+  delay: 2700,
 });
-
-var tl = new TweenMax.staggerFrom(
-  '.menu > div',
-  2,
-  {
-    opacity: 0,
-    y: 30,
-    ease: Expo.easeInOut,
-    delay: 4.2,
-  },
-  0.1,
-);
-
-var tl = new TweenMax.staggerFrom(
-  '.hero-container > div',
-  2,
-  {
-    opacity: 0,
-    y: 30,
-    ease: Expo.easeInOut,
-    delay: 4.2,
-  },
-  0.1,
-);
